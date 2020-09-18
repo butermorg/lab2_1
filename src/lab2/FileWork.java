3454cd6948749b3c80bdfdad4ab1c96a98b0f515
butermorg <badcawa@mail.ru>
2020-09-19 02:30:22 +0800

    complite

---
+++ src/lab2/FileWork.java
@@ -0,0 +1,29 @@
+package lab2;
+
+import java.io.*;
+
+
+public class FileWork {
+    String data;
+    String filename;
+    RandomAccessFile file;
+    public void Read() throws IOException {
+        filename = "123.txt";
+        file = new RandomAccessFile(new File(filename), "r");
+        //чтение информации из заданного файла
+        data = file.readLine();
+        file.close();
+        System.out.println("Информация из файла: " + data);
+}
+    public void found () throws IOException{
+    data = data.replaceAll("\\pP", "");
+        String[] arrayWords = data.split(" ");
+        //поиск по последней и первой букве
+            for (int i = 0; i < arrayWords.length-1; i++) {
+              if(Character.toLowerCase(arrayWords[i].charAt(arrayWords[i].length()-1)) == Character.toLowerCase(arrayWords[i+1].charAt(0))) {
+                  System.out.println(arrayWords[i] + " " + arrayWords[i+1]);
+              }
+    
+}
+    }
+}
